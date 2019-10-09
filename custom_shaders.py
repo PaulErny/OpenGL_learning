ec9e2d367fbb24348273ef6b7fa1e7ada4d2a3fe
ERNY Paul <ERNYP@saftbatteries.com>
2019-10-09 16:47:14 +0200

    added colors to the square

--- custom_shaders.py
+++ custom_shaders.py
@@ -10,21 +10,35 @@ def load_shaders():
     vertexShaderCode = """
             #version 330 core
 
+            // Notice that the "0" here equals the "0" in glVertexAttribPointer
             layout(location = 0) in vec3 vertexPosition_modelspace;
+            // Notice that the "1" here equals the "1" in glVertexAttribPointer
+            layout(location = 1) in vec3 vertexColor;
             
+            out vec3 fragmentColor;
             uniform mat4 MVP;
             
             void main() {
               gl_Position =  MVP * vec4(vertexPosition_modelspace,1);
+              
+              // The color of each vertex will be interpolated
+              // to produce the color of each fragment
+              fragmentColor = vertexColor;
             }
         """
 
     # fragment shader program
     fragmentShaderCode = """
             #version 330 core
+            
+            // Interpolated values from the vertex shaders
+            in vec3 fragmentColor;
             out vec3 color;
+            
             void main(){
-              color = vec3(1,0,0);
+              // Output color = color specified in the vertex shader,
+              // interpolated between all 3 surrounding vertices
+              color = fragmentColor;
             }
         """
 
--- scene.py
+++ scene.py
@@ -33,10 +33,8 @@ def main():
     glfw.set_input_mode(window, glfw.STICKY_KEYS, GL_TRUE)
 
     # OpenGL init
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluPerspective(80, 800 / 600, 0.1, 500)  # fov
     glEnable(GL_DEPTH_TEST)  # explicit
+    glDepthFunc(GL_LESS)
 
     # Vertex array obj creation
     vao = glGenVertexArrays(1)
@@ -80,7 +78,7 @@ def main():
                                 [1.0, -1.0, 1.0]],
                                dtype='f')
 
-    # Vertex buffer obj creation
+    # Vertex buffer obj creation for the cube
     vbo = glGenBuffers(1)
     glBindBuffer(GL_ARRAY_BUFFER, vbo)
     glBufferData(GL_ARRAY_BUFFER, sys.getsizeof(vertex_array), vertex_array, GL_STATIC_DRAW)
@@ -88,6 +86,49 @@ def main():
     # using custom shader to draw the vbo
     shader_program = custom_shaders.load_shaders()
     glUseProgram(shader_program)
+
+    color_array = numpy.array([[0.583,  0.771,  0.014],
+                               [0.609,  0.115,  0.436],
+                               [0.327,  0.483,  0.844],
+                               [0.822,  0.569,  0.201],
+                               [0.435,  0.602,  0.223],
+                               [0.310,  0.747,  0.185],
+                               [0.597,  0.770,  0.761],
+                               [0.559,  0.436,  0.730],
+                               [0.359,  0.583,  0.152],
+                               [0.483,  0.596,  0.789],
+                               [0.559,  0.861,  0.639],
+                               [0.195,  0.548,  0.859],
+                               [0.014,  0.184,  0.576],
+                               [0.771,  0.328,  0.970],
+                               [0.406,  0.615,  0.116],
+                               [0.676,  0.977,  0.133],
+                               [0.971,  0.572,  0.833],
+                               [0.140,  0.616,  0.489],
+                               [0.997,  0.513,  0.064],
+                               [0.945,  0.719,  0.592],
+                               [0.543,  0.021,  0.978],
+                               [0.279,  0.317,  0.505],
+                               [0.167,  0.620,  0.077],
+                               [0.347,  0.857,  0.137],
+                               [0.055,  0.953,  0.042],
+                               [0.714,  0.505,  0.345],
+                               [0.783,  0.290,  0.734],
+                               [0.722,  0.645,  0.174],
+                               [0.302,  0.455,  0.848],
+                               [0.225,  0.587,  0.040],
+                               [0.517,  0.713,  0.338],
+                               [0.053,  0.959,  0.120],
+                               [0.393,  0.621,  0.362],
+                               [0.673,  0.211,  0.457],
+                               [0.820,  0.883,  0.371],
+                               [0.982,  0.099,  0.879]],
+                              dtype='f')
+
+    # Vertex buffer obj creation for cube's color
+    color_buffer = glGenBuffers(1)
+    glBindBuffer(GL_ARRAY_BUFFER, color_buffer)
+    glBufferData(GL_ARRAY_BUFFER, sys.getsizeof(color_array), color_array, GL_STATIC_DRAW)
 
     # creation of MVP matrix
     projection = glm.perspective(glm.radians(80.0), 4/3, 0.1, 100)
@@ -108,11 +149,28 @@ def main():
         glUseProgram(shader_program)
         glUniformMatrix4fv(matrixID, 1, GL_FALSE, glm.value_ptr(mvp_matrix))
 
+        # color
+        glEnableVertexAttribArray(1)
+        glBindBuffer(GL_ARRAY_BUFFER, color_buffer)
+        glVertexAttribPointer(1,            # attribute. No particular reason for 1, but must match the layout in the shader.
+                              3,
+                              GL_FLOAT,
+                              GL_FALSE,
+                              0,
+                              None)
+        # draw cube
         glEnableVertexAttribArray(0)
         glBindBuffer(GL_ARRAY_BUFFER, vbo)
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, None)
+        glVertexAttribPointer(0,            # attribute. No particular reason for 0, but must match the layout in the shader.
+                              3,
+                              GL_FLOAT,
+                              GL_FALSE,
+                              0,
+                              None)
         glDrawArrays(GL_TRIANGLES, 0, 12*3)
         glDisableVertexAttribArray(0)
+        # color end
+        glDisableVertexAttribArray(1)
 
         glfw.swap_buffers(window)
 
