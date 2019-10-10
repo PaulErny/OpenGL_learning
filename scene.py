aed1c9bbaada78ef9c9e256c9bceecb2751107ec
ERNY Paul <ERNYP@saftbatteries.com>
2019-10-10 13:33:03 +0200

    arrays of vertices moved into obj.py

--- scene.py
+++ scene.py
@@ -1,102 +1,14 @@
-import glfw
 import logging
-import numpy
-from shape import *
-from ctypes import *
-import time
-from FreeCam import FreeCam
-import custom_shaders
+
 from OpenGL.GL import *
 from OpenGL.GLU import *
-import glm
+import glfw
+import numpy
+
+from shape import *
+import obj
 
 # visit https://rdmilligan.wordpress.com/2016/08/27/opengl-shaders-using-python/ for python OpenGL shader example
-
-triangle_vertices = numpy.array([[2.0, 0.0, -1.0],
-                                 [0.0, 0.0, -1.0],
-                                 [0.0,  2.0, -1.0]],
-                                dtype='f')
-
-triangle_color = numpy.array([[1.0, 0.0, 0.0],
-                              [1.0, 0.0, 0.0],
-                              [1.0,  0.0, 0.0]],
-                             dtype='f')
-
-cube_vertex_array = numpy.array([[-1.0, -1.0, -1.0],  # triangle 1 : begin
-                                 [-1.0, -1.0, 1.0],
-                                 [-1.0, 1.0, 1.0],  # triangle 1 : end
-                                 [1.0, 1.0, -1.0],  # triangle 2 : begin
-                                 [-1.0, -1.0, -1.0],
-                                 [-1.0, 1.0, -1.0],  # triangle 2 : end
-                                 [1.0, -1.0, 1.0],
-                                 [-1.0, -1.0, -1.0],
-                                 [1.0, -1.0, -1.0],
-                                 [1.0, 1.0, -1.0],
-                                 [1.0, -1.0, -1.0],
-                                 [-1.0, -1.0, -1.0],
-                                 [-1.0, -1.0, -1.0],
-                                 [-1.0, 1.0, 1.0],
-                                 [-1.0, 1.0, -1.0],
-                                 [1.0, -1.0, 1.0],
-                                 [-1.0, -1.0, 1.0],
-                                 [-1.0, -1.0, -1.0],
-                                 [-1.0, 1.0, 1.0],
-                                 [-1.0, -1.0, 1.0],
-                                 [1.0, -1.0, 1.0],
-                                 [1.0, 1.0, 1.0],
-                                 [1.0, -1.0, -1.0],
-                                 [1.0, 1.0, -1.0],
-                                 [1.0, -1.0, -1.0],
-                                 [1.0, 1.0, 1.0],
-                                 [1.0, -1.0, 1.0],
-                                 [1.0, 1.0, 1.0],
-                                 [1.0, 1.0, -1.0],
-                                 [-1.0, 1.0, -1.0],
-                                 [1.0, 1.0, 1.0],
-                                 [-1.0, 1.0, -1.0],
-                                 [-1.0, 1.0, 1.0],
-                                 [1.0, 1.0, 1.0],
-                                 [-1.0, 1.0, 1.0],
-                                 [1.0, -1.0, 1.0]],
-                                dtype='f')
-
-cube_color_array = numpy.array([[0.583, 0.771, 0.014],
-                                [0.609, 0.115, 0.436],
-                                [0.327, 0.483, 0.844],
-                                [0.822, 0.569, 0.201],
-                                [0.435, 0.602, 0.223],
-                                [0.310, 0.747, 0.185],
-                                [0.597, 0.770, 0.761],
-                                [0.559, 0.436, 0.730],
-                                [0.359, 0.583, 0.152],
-                                [0.483, 0.596, 0.789],
-                                [0.559, 0.861, 0.639],
-                                [0.195, 0.548, 0.859],
-                                [0.014, 0.184, 0.576],
-                                [0.771, 0.328, 0.970],
-                                [0.406, 0.615, 0.116],
-                                [0.676, 0.977, 0.133],
-                                [0.971, 0.572, 0.833],
-                                [0.140, 0.616, 0.489],
-                                [0.997, 0.513, 0.064],
-                                [0.945, 0.719, 0.592],
-                                [0.543, 0.021, 0.978],
-                                [0.279, 0.317, 0.505],
-                                [0.167, 0.620, 0.077],
-                                [0.347, 0.857, 0.137],
-                                [0.055, 0.953, 0.042],
-                                [0.714, 0.505, 0.345],
-                                [0.783, 0.290, 0.734],
-                                [0.722, 0.645, 0.174],
-                                [0.302, 0.455, 0.848],
-                                [0.225, 0.587, 0.040],
-                                [0.517, 0.713, 0.338],
-                                [0.053, 0.959, 0.120],
-                                [0.393, 0.621, 0.362],
-                                [0.673, 0.211, 0.457],
-                                [0.820, 0.883, 0.371],
-                                [0.982, 0.099, 0.879]],
-                               dtype='f')
 
 
 def main():
@@ -127,10 +39,10 @@ def main():
     vao = glGenVertexArrays(1)
     glBindVertexArray(vao)
 
-    cube = Shape(cube_vertex_array, cube_color_array)
+    cube = Shape(obj.cube_vertex_array, obj.cube_color_array)
     cube.init()
 
-    triangle = Shape(triangle_vertices, triangle_color)
+    triangle = Shape(obj.triangle_vertices, obj.triangle_color)
     triangle.init()
 
     while not glfw.window_should_close(window) and glfw.get_key(window, glfw.KEY_ESCAPE) != glfw.PRESS:
--- t.py
+++
@@ -1,7 +0,0 @@
-import numpy
-
-array = [0.7107949633841704, 0, -0.7033992607528785]
-res = numpy.multiply(array, 0.001828908920288086)
-print(res)
-res = numpy.multiply(array, 3.0)
-print(res)
